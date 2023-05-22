d08f3b9f52a11ac826900e1c6ccc5aeb15fc5320
nagatulasinani5241 <120091267+nagatulasinani5241@users.noreply.github.com>
2023-05-22 22:17:59 +0530

    #include<stdio.h> int main() {     int n,a[100],i,d,j;     scanf("%d",&n);     i=0;     while(n!=0)     {         d=n%10;         n=n/10;         a[i]=d;         i++;     }     for(j=i-1;j>=0;j--)     {         if(a[j]==6)         {             a[j]=9;             break;         }     }     for(j=i-1;j>=0;j--)     printf("%d",a[j]); }

---
+++ Maximum_69_Number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,d,j;
+    scanf("%d",&n);
+    i=0;
+    while(n!=0)
+    {
+        d=n%10;
+        n=n/10;
+        a[i]=d;
+        i++;
+    }
+    for(j=i-1;j>=0;j--)
+    {
+        if(a[j]==6)
+        {
+            a[j]=9;
+            break;
+        }
+    }
+    for(j=i-1;j>=0;j--)
+    printf("%d",a[j]);
+}
